94763cf72ed0670e3fbb2e09ad3dbd44463e4e72
Jesse Koch <thejessekoch@gmail.com>
2025-03-06 00:35:21 -0800

    fixed dumb bug with traversing expansions 💢

--- src/lib/classes/GameSession.ts
+++ src/lib/classes/GameSession.ts
@@ -1,56 +1,54 @@
-import Global from "./Global";
-import Redscrolls from "./Redscrolls";
+import Global from './Global';
+import Redscrolls from './Redscrolls';
 
 class GameSession {
-    instance: GameSession | null = null;
+	instance: GameSession | null = null;
 
-    constructor() {
-        if (this.instance) return this.instance;
-        this.instance = this;
-    }
+	constructor() {
+		if (this.instance) return this.instance;
+		this.instance = this;
+	}
 
-    static async getFullSession(id: string) {
-        // const char = await PocketBase.db.send("full_character/" + id);
+	static async getFullSession(id: string) {
+		// const char = await PocketBase.db.send("full_character/" + id);
 
-        const expansionFields = Global.tools.concatExpansionFields([
-            // Session
-            "quests",
-            "quests.plots",
-            "quests.plots.tags",
-            "chat_history",
+		const expansionFields = Global.tools.concatExpansionFields([
+			// Session
+			'quests',
+			'quests.plots',
+			'quests.plots.tags',
+			'chat_history',
 
-            // Character
-            "character",
-            "character.status",
-            "character.metadata",
+			// Character
+			'character',
+			'character.status',
+			'character.metadata',
 
-            // Moments
-            "character.moments",
-            "character.moments.tags",
+			// Moments
+			'character.moments',
+			'character.moments.tags',
 
-            // Inventory
-            "character.inventory",
-            "character.inventory.metadata",
+			// Inventory
+			'character.inventory',
+			'character.inventory.metadata',
 
-            // Items
-            "character.inventory.items",
-            "character.inventory.items.tags",
-        ])
+			// Items
+			'character.inventory.items',
+			'character.inventory.items.tags'
+		]);
 
-        const char = await Redscrolls.pb.collection("session").getOne(id, {
-            expand: expansionFields
-        })
+		const char = await Redscrolls.pb.collection('session').getOne(id, {
+			expand: expansionFields
+		});
 
+		let res = Global.tools.traverseExpansions(char);
+		res = Global.tools.cullFields(res, ['created', 'updated', 'collectionId', 'collectionName']);
+		console.log(res);
+		// res = Global.tools.objectMinifier(res);
+		// delete res.expand;
 
-
-        let res = Global.tools.traverseExpansions(char);
-        res = Global.tools.cullFields(res, ["created", "updated", "collectionId", "collectionName"]);
-        console.log(res);
-        // res = Global.tools.objectMinifier(res);
-        // delete res.expand;
-
-        return res;
-    }
+		return res;
+	}
 }
 
 export default GameSession;
--- src/routes/api/test/+server.ts
+++ src/routes/api/test/+server.ts
@@ -1,8 +1,11 @@
-import GameSession from "$lib/classes/GameSession";
-
+import GameSession from '$lib/classes/GameSession';
 
 export async function GET() {
-    const response = await GameSession.getFullSession("865j97c668vsi1y")
+	const response = await GameSession.getFullSession('865j97c668vsi1y');
 
-    return new Response(response)
-} 
+	return new Response(JSON.stringify(response), {
+		headers: {
+			'content-type': 'application/json'
+		}
+	});
+}
