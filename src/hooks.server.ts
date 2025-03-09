dd8f32bf6ae8dcb2fa8f182f5c0fbc3867c9fd8d
Jesse Koch <thejessekoch@gmail.com>
2025-03-08 18:05:14 -0800

    double checked init and serverpb

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -6,17 +6,14 @@ import PocketBase from 'pocketbase';
 export let serverPB: PocketBase;
 
 export const handle: Handle = async ({ event, resolve }) => {
-	console.log('Server handling');
 	const response = await resolve(event);
 	return response;
 };
 
 export const init: ServerInit = async () => {
-	console.log('Server initting', PRIVATE_SERVICE_EMAIL, PRIVATE_SERVICE_PASSWORD);
 	serverPB = new PocketBase(PUBLIC_POCKETBASE_URL);
 	await serverPB
 		.collection('users')
 		.authWithPassword(PRIVATE_SERVICE_EMAIL, PRIVATE_SERVICE_PASSWORD);
-	console.log('Server PB initialized');
 	console.log(serverPB.authStore.record);
 };
--- src/routes/api/me/+server.ts
+++
@@ -1,5 +0,0 @@
-import { serverPB } from '../../hooks.server';
-
-export const GET = async () => {
-	console.log('ME', serverPB.authStore.record);
-};
