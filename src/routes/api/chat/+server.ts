490d32faca4d7d9fb27a071686db2d53c1bf07ed
IzykGit <lancehemphill2000@gmail.com>
2025-03-08 19:17:10 -0500

    made migrations

--- src/routes/api/chat/+server.ts
+++ src/routes/api/chat/+server.ts
@@ -1,11 +1,11 @@
-import Redscrolls from "$lib/classes/Redscrolls";
+import Deepseek from "$lib/classes/Deepseek.js";
 
 export async function POST(event) {
     const body = await event.request.json()
 
     if (!body.message) return new Response(JSON.stringify({ message: "Did not find message" }), { status: 400 });
 
-    const deepseekResponse = await Redscrolls.deepseek.testCompletion(body.message);
+    const deepseekResponse = await Deepseek.testCompletion(body.message);
 
 
     return new Response(JSON.stringify(deepseekResponse), {
